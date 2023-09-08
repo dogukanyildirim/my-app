36b574137d4668b0652a0cd51edb457f555ecd7c
Dogukan Yildirim <dyildirim76@gmail.com>
2023-09-08 04:44:06 +0300

    final

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import "bulma/css/bulma.css";
 import Course from "@/components/Course";
 
 import Breakfast from "@/images/breakfast.jpg";
@@ -7,42 +8,63 @@ import Donut from "@/images/donut.jpg";
 import Macaron from "@/images/macaron.jpg";
 import Plumb from "@/images/plumb.jpg";
 
-
-
 export default function Home() {
   return (
     <main className="App">
-
-      <Course
-        image= {Breakfast}
-        title="breakfast"
-        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p"
-      />
-      <Course
-        image={Cherry}
-        title="cherry"
-        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p"
-      />
-      <Course
-        image={Coffee}
-        title="coffee"
-        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p"
-      />
-      <Course
-        image={Donut}
-        title="donut"
-        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p"
-      />
-      <Course
-        image={Macaron}
-        title="macaron"
-        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p"
-      />
-      <Course
-        image={Plumb}
-        title="plumb"
-        description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p"
-      />
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Fruits Project</p>
+        </div>
+      </section>
+      <div className="container">
+      <section className="section">
+        <div className="columns">
+          <div className="column">
+            <Course
+              image={Breakfast}
+              title="breakfast"
+              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p"
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Cherry}
+              title="cherry"
+              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p"
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Coffee}
+              title="coffee"
+              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p"
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Donut}
+              title="donut"
+              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p"
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Macaron}
+              title="macaron"
+              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p"
+            />
+          </div>
+          <div className="column">
+            <Course
+              image={Plumb}
+              title="plumb"
+              description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop p"
+            />
+          </div>
+        </div>
+      </section>
+      </div>
+      
     </main>
   );
 }
--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-
 import Image from "next/image";
 
 function Course({ image, title, description }) {
   return (
     <div>
-      <Image 
-      src={image}
-      width={250}
-      height={250}/>
-      <div> {title} </div>
-      <div> {description} </div>
+      <div className="card">
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <Image src={image} alt="Placeholder image" />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-content">
+              <p className="title is-4">{title}</p>
+              {/* <p className="subtitle is-6">this is subtitle</p> */}
+            </div>
+          </div>
+          <div className="content">{description}</div>
+        </div>
+      </div>
     </div>
   );
 }
